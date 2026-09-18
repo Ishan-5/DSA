5ae692cf836edd9cdd28d563767ab46e5fa78976
Ishan_18 <devansh.7711@gmail.com>
2026-09-18 23:35:18 +0530

    Time: 5 ms (87.19%) | Memory: 20.7 MB (19.85%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -2,5 +2,5 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         import re
         clean_s = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
-        return clean_s==clean_s[::-1]
-
+        reverse_s = clean_s[::-1]
+        return clean_s==reverse_s

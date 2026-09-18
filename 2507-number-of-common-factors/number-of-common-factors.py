fff6a1ba55ceb1084a43b44d7b11778ccd93337d
Ishan_18 <devansh.7711@gmail.com>
2026-09-19 03:00:46 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (56.51%) - LeetSync

--- 2507-number-of-common-factors/number-of-common-factors.py
+++ 2507-number-of-common-factors/number-of-common-factors.py
@@ -1,16 +1,7 @@
 class Solution:
     def commonFactors(self, a: int, b: int) -> int:
-        import math
-        gcd_num=math.gcd(a,b)
-        count=0
-        for i in range(1, int(math.sqrt(gcd_num)) + 1):
-            if gcd_num % i == 0:
-                count += 1            
-                if i * i != gcd_num:
-                    count += 1        
-                    
+        count = 0
+        for i in range(1,min(a,b)+1):
+            if(a%i==0 and b%i==0):
+                count+=1
         return count
-
-
-        
-

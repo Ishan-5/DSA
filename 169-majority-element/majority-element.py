9b8fa514ed842990f34f70f3f411fc19e375e0cd
Ishan_18 <devansh.7711@gmail.com>
2026-09-20 18:12:56 +0530

    Time: 3 ms (86.27%) | Memory: 21.6 MB (6.53%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,8 +1,11 @@
 class Solution:
     def majorityElement(self, nums: list[int]) -> int:
-        hash_map={}
-        n = len(nums)
-        for i in range (0,n):
-            hash_map[nums[i]] = hash_map.get(nums[i],0)+1
-            if hash_map[nums[i]] > n/2 :
-                return nums[i]
+        #hash_map={}
+        #n = len(nums)
+        #for i in range (0,n):
+        #    hash_map[nums[i]] = hash_map.get(nums[i],0)+1
+        #    if hash_map[nums[i]] > n/2 :
+        #        return nums[i]
+
+        nums.sort()
+        return nums[len(nums) // 2]

28a89353043e4935545a49e087c44433181d66dc
Ishan_18 <devansh.7711@gmail.com>
2026-09-20 17:02:39 +0530

    Time: 7 ms (41.72%) | Memory: 19.3 MB (52.86%) - LeetSync

--- 507-perfect-number/perfect-number.py
+++ 507-perfect-number/perfect-number.py
@@ -1,17 +1,15 @@
 class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
-        from math import sqrt
         if num <= 1:
             return False
-        result=[]
-        for i in range (1, int(sqrt(num))+1):
-            if num%i ==0:
-                result.append(i)
-                if num//i !=i:
-                    result.append(num//i)
-        result.remove(num)
-        total = sum(result)
-        if total == num:
-            return True
-        else :
-            return False
+            
+        from math import sqrt
+        total = 0 
+        
+        for i in range(1, int(sqrt(num)) + 1):
+            if num % i == 0:
+                total += i
+                if num // i != i:
+                    total += num // i
+                    
+        return (total - num) == num
